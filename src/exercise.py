d2913731410d895100043ee94cda34f4125fb32d
drknsblct <57183092+drknsblct@users.noreply.github.com>
2021-09-17 04:12:35 +0300

    Update exercise.py

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,9 @@
 def main():
     #write your code below this line
+    num = int(input('Give a year:'))
+    
+    if num < 2015:
+        print('Ancient history!')
 
 if __name__ == '__main__':
     main()
